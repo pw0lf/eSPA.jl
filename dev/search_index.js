75777e0ee13266acef2a6f33c89a760fa2d7db07
Documenter.jl <documenter@juliadocs.github.io>
2025-04-09 14:20:13 +0000

    build based on a50b051

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = eSPA","category":"page"},{"location":"#eSPA","page":"Home","title":"eSPA","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for eSPA.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [eSPA]","category":"page"}]
+}
