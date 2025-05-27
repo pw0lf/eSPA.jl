b942acab9ca20b2bfaeabac00572a5d663168059
Philipp Wolf <philippwolf99@icloud.com>
2025-05-27 20:20:43 +0200

    changed search space

--- exp/exp_darwin_goal.jl
+++ exp/exp_darwin_goal.jl
@@ -71,7 +71,7 @@ function train_and_eval_model(K,eps_CL,G,tol)
     return acc
 end
 ho = @hyperopt for i = iter,
-            K = 5:(minimum([100,Int(0.8*174)])),
+            K = 5:100,
             G = 1:(d-1),
             eps_CL = exp10.(LinRange(-1,2,1000)),
             tol = exp10.(-LinRange(1,10,1000))
