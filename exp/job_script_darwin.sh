153e10d84c882dab7793270e77fb28316c29cc89
Philipp Wolf <philippwolf99@icloud.com>
2025-05-27 19:47:16 +0200

    darwin exp plus

---
+++ exp/job_script_darwin.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#========[ + + + + Requirements + + + + ]========#
+#SBATCH -A m2_datamining
+#SBATCH -p smp
+#SBATCH -c 2
+#SBATCH -J darwin
+#SBATCH --time=0-08:00:00
+#SBATCH --mem 128G
+
+#========[ + + + + Environment + + + + ]========#
+module load lang/Julia
+
+#========[ + + + + Job Steps + + + + ]========#
+srun julia --project=../. exp1_darwin_plus.jl -i 50
